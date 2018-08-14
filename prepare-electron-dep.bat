2ad2c8ab1c0e80559a41ab912f4b77109daa37dc
liuxudong <liuxudong@hylink.net.cn>
2018-08-14 13:33:58 +0800

    添加屏幕取词混淆项

--- prepare-electron-dep.bat
+++ prepare-electron-dep.bat
@@ -16,6 +16,8 @@ REM uplaunch.js
 call javascript-obfuscator src/for-electron/crates/uplaunch.js --output public/for-electron/crates/uplaunch.js --compact true --control-flow-flattening true --dead-code-injection true
 REM upversion.js
 call javascript-obfuscator src/for-electron/crates/upversion.js --output public/for-electron/crates/upversion.js --compact true --control-flow-flattening true --dead-code-injection true
+REM huaci_handler.js
+call javascript-obfuscator src/for-electron/crates/huaci_handler.js --output public/for-electron/crates/huaci_handler.js --compact true --control-flow-flattening true --dead-code-injection true
 
 REM main_process.js
 call javascript-obfuscator main_process.js --output main_process.dist.js --compact true --control-flow-flattening true --dead-code-injection true
