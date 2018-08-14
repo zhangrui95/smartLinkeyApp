8138b04a6318e85619b85a9838af34eb8a6d4d47
zhang <zhangrui_web@163.com>
2018-08-14 20:42:20 +0800

    change

--- src/for-electron/templates/script.js
+++ src/for-electron/templates/script.js
@@ -1,26 +1,22 @@
 const { ipcRenderer } = require('electron');
 
-setInterval(() => {
-  console.log('+1s');
-}, 1000);
-
 function send_event(cid) {
   console.log(cid);
   ipcRenderer.send('huaci-choice', cid);
   document.getElementById('app').className = 'animated bounceOut';
-  setTimeout(() => {
-    window.close();
-  }, 1000);
+  // setTimeout(() => {
+  //   window.close();
+  // }, 1000);
 }
 
 ipcRenderer.on('please-close', () => {
   console.log('main tell me close');
-  window.close();
+  // window.close();
 });
 
-setTimeout(() => {
-  document.getElementById('app').className = 'animated fadeOut';
-}, 4000);
-setTimeout(() => {
-  window.close();
-}, 5000);
+// setTimeout(() => {
+//   document.getElementById('app').className = 'animated fadeOut';
+// }, 4000);
+// setTimeout(() => {
+//   window.close();
+// }, 5000);
