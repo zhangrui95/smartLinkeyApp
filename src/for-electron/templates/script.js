b34688860ed4543a65f011b750a33b2ac8bfa904
zhang <zhangrui_web@163.com>
2018-08-15 09:04:35 +0800

    change

--- src/for-electron/templates/script.js
+++ src/for-electron/templates/script.js
@@ -4,14 +4,14 @@ function send_event(cid) {
   console.log(cid);
   ipcRenderer.send('huaci-choice', cid);
   document.getElementById('app').className = 'animated bounceOut';
-  // setTimeout(() => {
-  //   window.close();
-  // }, 1000);
+  setTimeout(() => {
+    window.close();
+  }, 1000);
 }
 
 ipcRenderer.on('please-close', () => {
   console.log('main tell me close');
-  // window.close();
+  window.close();
 });
 
 // setTimeout(() => {
