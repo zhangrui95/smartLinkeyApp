bf367d9666a7551e190d39215dd68e739529407e
zhang <zhangrui_web@163.com>
2018-10-10 09:38:42 +0800

    add 2.0

--- src/components/SiderMenu/index.js
+++ src/components/SiderMenu/index.js
@@ -3,24 +3,25 @@ import React from 'react';
 import DrawerMenu from 'rc-drawer';
 import SiderMenu from './SiderMenu';
 
-const SiderMenuWrapper = props =>
-  props.isMobile ? (
-    <DrawerMenu
-      getContainer={null}
-      level={null}
-      handleChild={<i className="drawer-handle-icon" />}
-      onHandleClick={() => {
-        props.onCollapse(!props.collapsed);
-      }}
-      open={!props.collapsed}
-      onMaskClick={() => {
-        props.onCollapse(true);
-      }}
-    >
-      <SiderMenu {...props} collapsed={props.isMobile ? false : props.collapsed} />
-    </DrawerMenu>
-  ) : (
-    <SiderMenu {...props} />
-  );
+const SiderMenuWrapper = props => (
+  // props.isMobile ? (
+  //   <DrawerMenu
+  //     getContainer={null}
+  //     level={null}
+  //     handleChild={<i className="drawer-handle-icon" />}
+  //     onHandleClick={() => {
+  //       props.onCollapse(!props.collapsed);
+  //     }}
+  //     open={!props.collapsed}
+  //     onMaskClick={() => {
+  //       props.onCollapse(true);
+  //     }}
+  //   >
+  //     <SiderMenu {...props} collapsed={props.isMobile ? false : props.collapsed} />
+  //   </DrawerMenu>
+  // ) : (
+  <SiderMenu {...props} />
+);
+// );
 
 export default SiderMenuWrapper;
