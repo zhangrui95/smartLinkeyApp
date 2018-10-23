a01226ed1a51523425614164222bab615bfd1f16
zhang <zhangrui_web@163.com>
2018-10-23 09:19:02 +0800

    change

--- src/routes/User/TokenLogin.js
+++ src/routes/User/TokenLogin.js
@@ -8,10 +8,10 @@ import { ipcRenderer } from 'electron';
 }))
 export default class TokenLogin extends Component {
   componentDidMount() {
-    console.log('auto-login=======')
+    console.log('auto-login=======');
     ipcRenderer.on('auto-login', this.autoLogin);
   }
-  autoLogin = (event, data) =>{
+  autoLogin = (event, data) => {
     this.props.dispatch({
       type: 'login/loginToken',
       payload: {
@@ -19,7 +19,6 @@ export default class TokenLogin extends Component {
         sid: 'Smartlinkey_sys',
       },
       callback: response => {
-        response.data.password = hex_md5(values.password);
         let userJson = JSON.stringify(response.data);
         sessionStorage.setItem('user', userJson);
         ipcRenderer.send('login-success');
@@ -28,8 +27,8 @@ export default class TokenLogin extends Component {
     this.props.dispatch({
       type: 'login/getLogin',
     });
-  }
-  render(){
-    return(<div></div>)
+  };
+  render() {
+    return <div />;
   }
 }
